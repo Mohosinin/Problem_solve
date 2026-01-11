3b10555da33b012b331597cae45fbf0ec38ede3a
Mohosinin Islam <mohosinin.islam34@gmail.com>
2026-01-11 11:45:53 +0600

    problem-1

--- poroblem-1.js
+++ poroblem-1.js
@@ -1,1 +1,10 @@
-function()
+function reverseString(str){
+    let reversed = '';
+    for(let i = str.length - 1; i >=0; i--){
+        reversed = reversed + str[i];
+    }
+    return reversed;
+}
+const input = "Hello, World!";
+const output = reverseString(input);
+console.log(output);
